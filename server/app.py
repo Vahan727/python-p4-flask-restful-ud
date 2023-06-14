a15c2a4bfeff6927fa91ff5c7a4a175b47e3ecb7
Vahan Nadjarian <vahan@nadjarian.com>
2023-06-14 15:50:00 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -79,6 +79,30 @@ class NewsletterByID(Resource):
         )
 
         return response
+    
+    def patch(self, id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        resp_dict = record.to_dict()
+        resp = make_response(resp_dict, 200)
+
+        return resp
+    
+    def delete(self, id):
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        resp = make_response(response_dict, 200)
+
+        return resp
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
